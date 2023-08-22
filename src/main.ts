5362d71c4aae9898826826a08385b229d2eb9adf
HADDANIAbdu <142311995+HADDANIAbdu@users.noreply.github.com>
2023-08-22 02:23:59 +0100

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template.html',
+  styleUrls: ['./global_styles.css'],
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
